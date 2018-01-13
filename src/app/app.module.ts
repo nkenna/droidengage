615c5aa9fcbf55c7b489138456212f88c4ebfb4c
ugwum <nnadiug@rocketmail.com>
2018-01-14 00:33:56 +0100

    made some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
+import { Http } from '@angular/http';
+import { WpApiModule, WpApiLoader, WpApiStaticLoader } from 'wp-api-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -8,6 +12,11 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+export function WpApiLoaderFactory(http: Http) {
+  return new WpApiStaticLoader(http, 'http://droidengage.com/wp-json/', /* namespace is optional, default: '/wp/v2' */);
+}
 
 @NgModule({
   declarations: [
@@ -18,6 +27,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule,    
+    WpApiModule.forRoot({
+      provide: WpApiLoader,
+      useFactory: (WpApiLoaderFactory),
+      deps: [Http]
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +43,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    InAppBrowser
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,87 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import { WpApiPosts } from 'wp-api-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  posts: Array<{excerpt: any, link: string, title: string}>;
+  postBody: any;
 
+  constructor(private iab: InAppBrowser, public http: Http, private wpApiPosts: WpApiPosts, public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+    this.allPosts();
+  }
+
+
+  allPosts(): any{
+    this.posts = [];
+  
+
+    this.wpApiPosts.getList()
+      .toPromise()
+      .then((response) => {
+        console.log(response.json());
+        this.postBody = response.json();
+        console.log(this.postBody[0].excerpt.rendered);
+
+        for(let i = 0; i <= this.postBody.length; i++){
+         let items = this.postBody[i];
+
+        
+
+          this.posts.push({
+             excerpt: this.trimExcerpt(this.postBody[i].excerpt.rendered),
+            link: this.postBody[i].link,
+            title: this.postBody[i].title.rendered 
+          });
+          console.log(this.posts);
+          //console.log(PostItem);
+
+          
+        }
+
+       
+      }
+        )
+      .then(body => {
+        console.log(this.posts);
+      })
+      .catch(error => {});
+
+      
+  }
+
+  //trim the excerpt to 20 characters so as to look nice
+  trimExcerpt(word: string) {
+    var len = 20;
+
+    var firstTrim = word.substring(3, word.length);
+    var secondTrim = word.length > len ? firstTrim.substring(0, 30) +  "...": word;
+
+    return secondTrim;
+  }
+
+  openPost(url: string){
+    this.iab.create(url);
   }
 
 }
